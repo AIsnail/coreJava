88283abd99f9bcaab46916d0aea6f20006d04e23
wang <wangshisen2021@163.com>
2022-08-19 23:44:33 +0800

    堆栈轨迹，部分代码

---
+++ src/v1ch6/stackTrace/StackTraceTest.java
@@ -0,0 +1,20 @@
+package v1ch6.stackTrace;
+
+
+/**
+ * A program that displays a trace feature of a recursive method call.
+ * @version 0.1 2022-08-19
+ * @author AIsnail
+ */
+public class StackTraceTest
+{
+    public static int factorial(int n)
+    {
+        
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
